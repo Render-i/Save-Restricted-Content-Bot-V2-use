2d9dfcca78c7ce2e8db5f9b9940d2eb3ff31bdff
Render-i <pleasemegoo@outlook.com>
2024-07-22 19:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "23937359"))
+API_HASH = getenv("API_HASH", "b5667982a25e4f8706e4ac75f92f995a")
+BOT_TOKEN = getenv("BOT_TOKEN", "7296574210:AAHTP0QAVOr5QbvZ9bIFDQix5cLdep3789E")
+OWNER_ID = int(getenv("OWNER_ID", "7315008850"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://formangodb2blip763:hellomango2@mymain.yjvosiy.mongodb.net/?retryWrites=true&w=majority&appName=Mymain")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002246457581"))
+FORCESUB = getenv("FORCESUB", "channelforyusl")
