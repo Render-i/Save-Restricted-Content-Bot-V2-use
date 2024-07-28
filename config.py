467622a5bf8992132ded8b36471b14293118a1a3
Render-i <pleasemegoo@outlook.com>
2024-07-28 10:59:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "23937359"))
-API_HASH = getenv("API_HASH", "b5667982a25e4f8706e4ac75f92f995a")
-BOT_TOKEN = getenv("BOT_TOKEN", "7296574210:AAHTP0QAVOr5QbvZ9bIFDQix5cLdep3789E")
-OWNER_ID = int(getenv("OWNER_ID", "7315008850"))
+API_ID = int(getenv("API_ID", "21503676"))
+API_HASH = getenv("API_HASH", "6c480b0a5221c80a5bda833c4bea259c")
+BOT_TOKEN = getenv("BOT_TOKEN", "7238867445:AAGePHfsyGLiJotJg-qRLXkn6HZ4h3Z_b64")
+OWNER_ID = int(getenv("OWNER_ID", "7396928828"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://formangodb2blip763:hellomango2@mymain.yjvosiy.mongodb.net/?retryWrites=true&w=majority&appName=Mymain")
-LOG_GROUP = int(getenv("LOG_GROUP", "-1002246457581"))
-FORCESUB = getenv("FORCESUB", "channelforyusl")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002219822240"))
+FORCESUB = getenv("FORCESUB", "hellochannelforfun")
